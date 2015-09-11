c350644632ec4c17009ddf54cb3333de7a185aeb
Julie David <julie.angela.david@gmail.com>
2015-09-11 13:56:16 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+#Employee Reviews
+
+Gives good employees raises.
---
+++ employee_reviews_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './department.rb'
+require './employee.rb'
+
+class EmployeeReviewsTest < Minitest::Test
+
+  def test_initialize
+
+  end
+
+end
